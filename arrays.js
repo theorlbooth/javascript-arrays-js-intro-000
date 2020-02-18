f8e710ced75bfaf543acf295ba4daccd55914098
Theo Booth <theorlbooth@googlemail.com>
2020-02-18 16:51:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, newelement) {
+  return [newelement, ...array]
+}
+
+function destructivelyAddElementToEndOfArray (array, newelement) {
+  newarray = [newelement, ...array]
+  return newarray
+}
