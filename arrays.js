5a0e926161eac98fc56e87a70f633b9148de6cf7
Theo Booth <theorlbooth@googlemail.com>
2020-02-18 16:53:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,16 @@ function addElementToBeginningOfArray(array, newelement) {
   return [newelement, ...array]
 }
 
-function destructivelyAddElementToEndOfArray (array, newelement) {
+function destructivelyAddElementToBeginningOfArray (array, newelement) {
   newarray = [newelement, ...array]
   return newarray
+}
+
+function addElementToEndOfArray (array, newelement) {
+  return [...array, newelement]
+}
+
+function destructivelyAddElementToEndOfArray(array, newelement) {
+  newarray = [...array, newelement]
+  return newarray  
 }
